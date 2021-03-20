73c0f5d9c8e382a735c14dc0c074739c32f85732
名前しょちゃんです <redforce@namaeshochandesunoMacBook-Pro.local>
2021-03-21 00:28:30 +0900

    create new rspec file

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user)  { create(:user)}
+
+  describe 'ユーザー登録が可能な場合' do
+    it '新規登録できること' do
+      expect(user).to be_valid
+    end
+  end
+
+  describe 'ユーザー名のバリデーションを検証する場合' do
+    it 'ユーザー名が入力されていないと登録できないこと' do
+      user.name = nil
+      user.valid?
+      expect(user.errors[:name]).to include('を入力してください')
+    end
+  end
+
+  describe 'メールアドレスのバリデーションを検証する場合' do
+    it 'メールアドレスが入力されていないと登録できないこと' do
+      user.email = nil
+      user.valid?
+      expect(user.errors[:email]).to include('を入力してください')
+    end
+  end
+
+  describe 'パスワードのバリデーションを検証する場合' do
+    it 'パスワードと確認用パスワードが一致していないと無効な状態であること' do
+      user.password = 'password'
+      user.password_confirmation = 'invalid_password'
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include('とパスワードの入力が一致しません')
+    end
+
+    it 'パスワードが6文字未満なら無効な状態であること' do
+      user.password = user.password_confirmation = 'a' * 5
+      user.valid?
+      expect(user.errors[:password]).to include('は6文字以上で入力してください')
+    end
+  end
+end
+
---
+++ spec/requests/reviews_request_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Reviews", type: :request do
+  describe '#show' do
+    let(:user)  { create(:user) }
+    let(:review)  { create(:review, user: user) }
+    
+
+    before do 
+      sign_in user
+      get review_path(review.id)
+    end
+
+    it '正しく接続できること' do
+      expect(response).to have_http_status "200"
+    end
+
+    it "showページにreview.nameが含まれていること" do
+      expect(response.body).to include review.name
+    end
+
+    it "showページにreview.priceが含まれていること" do
+      expect(response.body).to include review.price.to_s
+    end
+
+    it "showページにreview.descriptionが含まれていること" do
+      expect(response.body).to include review.description
+    end
+  end
+end
