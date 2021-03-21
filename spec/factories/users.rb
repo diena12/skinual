04653fb64201438d5cc226f07f1ae152a33db035
名前しょちゃんです <redforce@namaeshochandesunoMacBook-Pro.local>
2021-03-22 01:20:24 +0900

    add calam(skin_type profile)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
     email { |n| "tester#{n}@skinual.jp" }
     password { 'password' }
     password_confirmation { 'password' }
+    skin_type { '乾燥肌' }
+    profile { '初めまして。' }
   end
 end
