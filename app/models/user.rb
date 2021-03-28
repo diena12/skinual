c05fb425e9d668b0d76dab402caf4cf7068f0625
名前しょちゃんです <redforce@namaeshochandesunoMacBook-Pro.local>
2021-03-29 00:28:26 +0900

    commentモデルと maesのアソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reviews, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :like_reviews, through: :likes, source: :review
+  has_many :comments, dependent: :destroy
+  has_one_attached :image
+
+  validates :name, presence: true
 end
