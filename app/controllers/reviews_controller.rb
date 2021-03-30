4a26a9139713f770cef5cfbc7ab3c22a9b288313
名前しょちゃんです <redforce@namaeshochandesunoMacBook-Pro.local>
2021-03-30 09:12:14 +0900

    destroyアクションの追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -39,6 +39,12 @@ class ReviewsController < ApplicationController
     redirect_to review_path(@review)
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path
+  end
+
   def ranking
     @all_ranks = Review.find(Like.group(:review_id).order('count(review_id) desc').limit(3).pluck(:review_id))
   end
