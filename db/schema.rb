61e04b412cd92ceb2ae8040ee5f86cee9c9b532e
名前しょちゃんです <redforce@namaeshochandesunoMacBook-Pro.local>
2021-04-01 00:53:47 +0900

    userモデルにadminカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_133745) do
+ActiveRecord::Schema.define(version: 2021_03_30_135806) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2021_03_17_133745) do
     t.datetime "updated_at", null: false
     t.string "skin_type"
     t.text "profile"
+    t.boolean "admin_flg"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
