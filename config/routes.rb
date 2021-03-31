ed0658296e284a25d16b54b8a3348d829d668c35
名前しょちゃんです <redforce@namaeshochandesunoMacBook-Pro.local>
2021-04-01 00:52:45 +0900

    review#searchのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'skinual#index'
   # Devise
   devise_for :users
+  devise_scope :user do
+    post '/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Skinual
   get '/',                        to: 'skinual#index'
@@ -13,13 +17,13 @@ Rails.application.routes.draw do
   resources :reviews do
     collection do
       get 'get_category_children', defaults: { format: 'json' }
+      get 'search'
     end
   end
   get '/reviews/:id',             to: 'reviews#show'
   get '/reviews',                 to: 'reviews#index'
   # Ranking
   get '/ranking',                 to: 'reviews#ranking'
-  
 
   # Category
   get '/categories/:id',          to: 'categories#show'
