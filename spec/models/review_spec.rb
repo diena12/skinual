c7cd97b96f99f3dc9eb9cdf3a99474d4ca94f268
名前しょちゃんです <redforce@namaeshochandesunoMacBook-Pro.local>
2021-04-05 00:21:59 +0900

    reviewモデルのテスト追加

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:review) { create(:review) }
+
+  describe 'reviewのバリデーションを検証する場合' do
+    it '製品名が入力されていないと登録できないこと' do
+      review.name = nil
+      review.valid?
+      expect(review.errors[:name]).to include('を入力してください')
+    end
+
+    it '紹介文が入力されていないと登録できないこと' do
+      review.description = nil
+      review.valid?
+      expect(review.errors[:description]).to include('を入力してください')
+    end
+
+    it '質感が入力されていないと登録できないこと' do
+      review.texture = nil
+      review.valid?
+      expect(review.errors[:texture]).to include('を入力してください')
+    end
+
+    it '価格が入力されていないと登録できないこと' do
+      review.price = nil
+      review.valid?
+      expect(review.errors[:price]).to include('を入力してください')
+    end
+  end
 end
